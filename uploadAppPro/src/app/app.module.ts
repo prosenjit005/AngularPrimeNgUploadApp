2b044d3f2f83a14dfe41660356e3728c7db1d25f
prosenjit005 <prosenjit.jsr@gmail.com>
2022-08-27 18:07:19 +0530

    installed primeNG

--- uploadAppPro/src/app/app.module.ts
+++ uploadAppPro/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
